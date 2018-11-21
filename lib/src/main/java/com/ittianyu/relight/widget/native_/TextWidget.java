623d2f7b0c7a4bd2952a4c0d77953238ab22d961
liyujiang <liyujiang_tk@yeah.net>
2018-11-21 17:37:23 +0800

    add ImageWidget; add some props/method of BaseAndroidWidget; add some props of TextWidget;

--- lib/src/main/java/com/ittianyu/relight/widget/native_/TextWidget.java
+++ lib/src/main/java/com/ittianyu/relight/widget/native_/TextWidget.java
@@ -1,6 +1,8 @@
 package com.ittianyu.relight.widget.native_;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
 import android.util.TypedValue;
 import android.widget.TextView;
 
@@ -8,12 +10,16 @@ import android.arch.lifecycle.Lifecycle;
 
 public class TextWidget extends BaseAndroidWidget<TextView> {
     protected int gravity = -1;
-    protected String text;
+    protected CharSequence text;
     protected int textColor = -1;
     protected int textSize = -1;
     protected int lines = -1;
     protected int maxLines = -1;
-
+    protected int drawablePadding = -1;
+    protected Drawable drawableBottom = null;
+    protected Drawable drawableLeft = null;
+    protected Drawable drawableRight = null;
+    protected Drawable drawableTop = null;
 
     public TextWidget(Context context, Lifecycle lifecycle) {
         super(context, lifecycle);
@@ -42,9 +48,14 @@ public class TextWidget extends BaseAndroidWidget<TextView> {
         textSize(textSize);
         lines(lines);
         maxLines(maxLines);
+        drawablePadding(drawablePadding);
+        drawableLeft(drawableLeft);
+        drawableTop(drawableTop);
+        drawableRight(drawableRight);
+        drawableBottom(drawableBottom);
     }
 
-    public TextWidget text(String text) {
+    public TextWidget text(CharSequence text) {
         this.text = text;
         if (null != text)
             view.setText(text);
@@ -83,6 +94,73 @@ public class TextWidget extends BaseAndroidWidget<TextView> {
         this.maxLines = maxLines;
         if (maxLines != -1)
             view.setMaxLines(maxLines);
+        return this;
+    }
+
+    public TextWidget drawablePadding(int res) {
+        drawablePadding = res;
+        if (drawablePadding != -1)
+            view.setCompoundDrawablePadding(drawablePadding);
+        return this;
+    }
+
+    public TextWidget drawableLeft(@DrawableRes int res) {
+        return drawableLeft(drawable(res));
+    }
+
+    public TextWidget drawableLeft(Drawable drawable) {
+        drawableLeft = drawable;
+        if (drawableLeft != null) {
+            drawableLeft.setBounds(0, 0, drawableLeft.getMinimumWidth(),
+                    drawableLeft.getMinimumHeight());
+            Drawable[] drawables = view.getCompoundDrawables();
+            view.setCompoundDrawables(drawableLeft, drawables[1], drawables[2], drawables[3]);
+        }
+        return this;
+    }
+
+    public TextWidget drawableTop(@DrawableRes int res) {
+        return drawableTop(drawable(res));
+    }
+
+    public TextWidget drawableTop(Drawable drawable) {
+        drawableTop = drawable;
+        if (drawableTop != null) {
+            drawableTop.setBounds(0, 0, drawableTop.getMinimumWidth(),
+                    drawableTop.getMinimumHeight());
+            Drawable[] drawables = view.getCompoundDrawables();
+            view.setCompoundDrawables(drawables[0], drawableTop, drawables[2], drawables[3]);
+        }
+        return this;
+    }
+
+    public TextWidget drawableRight(@DrawableRes int res) {
+        return drawableRight(drawable(res));
+    }
+
+    public TextWidget drawableRight(Drawable drawable) {
+        drawableRight = drawable;
+        if (drawableRight != null) {
+            drawableRight.setBounds(0, 0, drawableRight.getMinimumWidth(),
+                    drawableRight.getMinimumHeight());
+            Drawable[] drawables = view.getCompoundDrawables();
+            view.setCompoundDrawables(drawables[0], drawables[1], drawableRight, drawables[3]);
+        }
+        return this;
+    }
+
+    public TextWidget drawableBottom(@DrawableRes int res) {
+        return drawableBottom(drawable(res));
+    }
+
+    public TextWidget drawableBottom(Drawable drawable) {
+        drawableBottom = drawable;
+        if (drawableBottom != null) {
+            drawableBottom.setBounds(0, 0, drawableBottom.getMinimumWidth(),
+                    drawableBottom.getMinimumHeight());
+            Drawable[] drawables = view.getCompoundDrawables();
+            view.setCompoundDrawables(drawables[0], drawables[1], drawables[2], drawableBottom);
+        }
         return this;
     }
 
