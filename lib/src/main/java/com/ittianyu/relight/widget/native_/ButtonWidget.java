8221eb13485dafefaa4432d5cca9e8a817dbc659
ittianyu <86839868@qq.com>
2018-12-25 02:11:47 +0800

    support xml 2.

--- lib/src/main/java/com/ittianyu/relight/widget/native_/ButtonWidget.java
+++ lib/src/main/java/com/ittianyu/relight/widget/native_/ButtonWidget.java
@@ -2,7 +2,6 @@ package com.ittianyu.relight.widget.native_;
 
 import android.arch.lifecycle.Lifecycle;
 import android.content.Context;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 public class ButtonWidget extends BaseTextWidget<Button, ButtonWidget> {
@@ -18,12 +17,5 @@ public class ButtonWidget extends BaseTextWidget<Button, ButtonWidget> {
     @Override
     public Button createView(Context context) {
         return new Button(context);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        ViewGroup.LayoutParams lp = view.getLayoutParams();
-        System.out.println("lp:" + lp);
     }
 }
